d8558a2aa807c59a592818dd1ad325114bffc837
YannMallegol <yann.mallegol@isen-bretagne.fr>
2015-08-26 12:05:21 -0400

    update

--- python_test/test2.py
+++ python_test/test2.py
@@ -1,7 +1,16 @@
 import pydicom, os
 
 for element in os.listdir('/neuro/users/yann.mallegol/Documents/internship/chris-db/python_test'):
-     if element.endswith('.dcm'):
-         print("'%s' est un fichier dicom" % element)
-     else:
-         print("'%s' n'est pas un fichier dicom" % element)
+	 if element.endswith('.dcm'):
+		 print("'%s' est un fichier dicom" % element)
+		 ds = pydicom.read_file(element)
+		 print(ds.PatientName)
+		 try:
+				print(ds.SeriesDescription)
+		 except NameError:
+			print('not possible to know this information')
+		 print(ds.SeriesNumber)
+	
+		 
+	 else:
+		 print("'%s' n'est pas un fichier dicom" % element)
