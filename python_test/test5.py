6c890f0025598b19f711c4455a75b2c1de9fad89
YannMallegol <yann.mallegol@isen-bretagne.fr>
2015-08-26 16:19:54 -0400

    new test

--- python_test/test5.py
+++ python_test/test5.py
@@ -3,6 +3,3 @@ import os, pydicom
 for dossier, sous_dossiers, fichiers in os.walk('/neuro/users/chris/data'):
     for fichier in fichiers:
         print(os.path.join(fichier))
-        #ds = pydicom.read_file(os.path.join(fichier))
-        #print(ds.PatientName)
-        #os.path.basename(path)
