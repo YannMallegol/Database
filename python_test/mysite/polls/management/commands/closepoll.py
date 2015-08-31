94028f423616755c42a7c197d911e169830a2704
YannMallegol <yann.mallegol@isen-bretagne.fr>
2015-08-31 09:37:50 -0400

    update

---
+++ python_test/mysite/polls/management/commands/closepoll.py
@@ -0,0 +1,26 @@
+import os, pydicom
+from django.core.management.base import BaseCommand, CommandError
+
+
+class Command(BaseCommand):
+    help = 'that does my stuff'
+
+    def handle(self, *args, **options):
+       for dossier, sous_dossiers, fichiers in os.walk('/neuro/users/chris/data'):
+           for fichier in fichiers:
+               fullpath = os.path.join(dossier, fichier)
+               print(fullpath)
+               try:
+                  ds = pydicom.read_file(fullpath)
+                  print(ds.PatientName)
+                  try:
+                      print(ds.SeriesDescription)
+                  except NameError:
+                      print('not possible to know this information')
+                  try:
+                      print(ds.SeriesNumber)
+                  except NameError:
+                      print('not possible to know this information')
+
+               except pydicom.errors.InvalidDicomError:
+                       print('not possible to know this information')
